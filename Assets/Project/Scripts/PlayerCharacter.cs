a66af938e1f858bc5ebf78277ac7d464aaea169f
Taro Omiya <taro.omiya@omiyagames.com>
2017-12-02 14:41:20 -0500

    Finally got the camera done.

--- Assets/Project/Scripts/PlayerCharacter.cs
+++ Assets/Project/Scripts/PlayerCharacter.cs
@@ -1,16 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
-public class PlayerCharacter : MonoBehaviour {
+namespace LudumDare40 {
+	[RequireComponent(typeof(Animator))]
+	[RequireComponent(typeof(Sprite))]
+	[RequireComponent(typeof(Rigidbody2D))]
+	public class PlayerCharacter : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		[SerializeField]
+		float movementSpeed;
+
+		Vector2 controls = Vector2.zero;
+		Vector2 move = Vector2.zero;
+		Rigidbody2D body = null;
+
+		public Rigidbody2D Body {
+			get {
+				return body;
+			}
+		}
+
+		void Start() {
+			body = GetComponent<Rigidbody2D> ();
+		}
+
+		// Update is called once per frame
+		void Update () {
+			// Get the controls
+			controls.x = CrossPlatformInputManager.GetAxis("Horizontal");
+			controls.y = CrossPlatformInputManager.GetAxis("Vertical");
+
+			// Normalize directions (diagonals are the same speed as normal controls)
+			if ((Mathf.Approximately (controls.x, 0) == false) || (Mathf.Approximately (controls.y, 0) == false)) {
+				controls.Normalize ();
+			}
+			//Debug.Log (controls);
+		}
+
+		void FixedUpdate() {
+			// Multiply by movement speed
+			move.x = controls.x * movementSpeed * Time.deltaTime;
+			move.y = controls.y * movementSpeed * Time.deltaTime;
+
+			// Apply force on the character
+			if (move.sqrMagnitude > 0) {
+				body.velocity = move;
+			}
+		}
 	}
 }
