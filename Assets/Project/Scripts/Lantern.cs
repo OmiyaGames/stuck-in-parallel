c9fb48f3d27d4ec84abed0e1a1f7b1beafe4b479
Taro Omiya <taro.omiya@omiyagames.com>
2017-12-03 01:38:08 -0500

    Spending a lot of time of fixing collision.  A reset button will be needed on every level.

---
+++ Assets/Project/Scripts/Lantern.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LudumDare40 {
+	public enum Universe {
+		Default,
+		Green,
+		Purple,
+		Orange
+	}
+
+	public class Lantern : MonoBehaviour {
+		[SerializeField]
+		Collider collider3d;
+		[SerializeField]
+		Collider2D collider2d;
+		[SerializeField]
+		Universe universe;
+
+		public Universe lastUnivese;
+
+		public Collider Collider3d {
+			get {
+				return collider3d;
+			}
+		}
+
+		public Collider2D Collider2d {
+			get {
+				return collider2d;
+			}
+		}
+
+		public Universe Universe {
+			get {
+				return universe;
+			}
+		}
+
+		// Use this for initialization
+		void Start () {
+			PlayerCollider.AddLantern (this);
+			lastUnivese = universe;
+		}
+	}
+}
