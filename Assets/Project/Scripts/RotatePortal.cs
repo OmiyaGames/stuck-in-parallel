45c58673507308a3478c0b51e87d8ffda7260af3
Taro Omiya <taro.omiya@omiyagames.com>
2017-12-03 12:22:03 -0500

    Making it possible to pick-up a single lantern.  Unable to drop it right now.

--- Assets/Project/Scripts/RotatePortal.cs
+++ Assets/Project/Scripts/RotatePortal.cs
@@ -12,9 +12,14 @@ namespace LudumDare40 {
 		[SerializeField]
 		Transform spinObject;
 
+//		[Header("In Inventory")]
+//		[SerializeField]
+//		float diffMultiplier = 100f;
+
 		Rigidbody2D body;
 		float rotateSpeed;
 		Vector3 rotateDirection;
+//		Vector2 lastPosition;
 
 		// Use this for initialization
 		void Start () {
@@ -24,10 +29,22 @@ namespace LudumDare40 {
 		
 		// Update is called once per frame
 		void Update () {
-			rotateSpeed = Mathf.Clamp((body.velocity.x * -velocityToSpin * Time.deltaTime), -maxSpin, maxSpin);
+			float x = body.velocity.x;
+			float y = body.velocity.y;
+//			if (PlayerInventory.HoldingLantern == this) {
+//				x = (body.position.x - lastPosition.x) * diffMultiplier;
+//				y = (body.position.y - lastPosition.y) * diffMultiplier;
+//			}
+
+			// Spin the object
+			rotateSpeed = Mathf.Clamp((x * -velocityToSpin * Time.deltaTime), -maxSpin, maxSpin);
 			spinObject.RotateAround (spinObject.position, Vector3.up, rotateSpeed);
-			rotateSpeed = Mathf.Clamp((body.velocity.y * velocityToSpin * Time.deltaTime), -maxSpin, maxSpin);
+			rotateSpeed = Mathf.Clamp((y * velocityToSpin * Time.deltaTime), -maxSpin, maxSpin);
 			spinObject.RotateAround (spinObject.position, Vector3.right, rotateSpeed);
+
+			// Update last position
+//			lastPosition.x = body.position.x;
+//			lastPosition.y = body.position.y;
 		}
 	}
 }
