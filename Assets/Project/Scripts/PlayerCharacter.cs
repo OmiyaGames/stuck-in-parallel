611a3e0d4b7c63bffa796640c0261360b3dab261
Taro Omiya <taro.omiya@omiyagames.com>
2017-12-03 14:24:07 -0500

    Got together an inventory of sorts.

--- Assets/Project/Scripts/PlayerCharacter.cs
+++ Assets/Project/Scripts/PlayerCharacter.cs
@@ -8,6 +8,17 @@ namespace LudumDare40 {
 	[RequireComponent(typeof(Sprite))]
 	[RequireComponent(typeof(Rigidbody2D))]
 	public class PlayerCharacter : MonoBehaviour {
+		const string LastDirection = "LastDirection";
+		const string Horizontal = "Horizontal";
+		const string Vertical = "Vertical";
+		const string Velocity = "Velocity";
+
+		public enum Direction {
+			Down,
+			Up,
+			Left,
+			Right
+		}
 
 		[SerializeField]
 		float movementSpeed;
@@ -15,6 +26,8 @@ namespace LudumDare40 {
 		Vector2 controls = Vector2.zero;
 		Vector2 move = Vector2.zero;
 		Rigidbody2D body = null;
+		Animator animator = null;
+		Direction facing = Direction.Down;
 
 		public Rigidbody2D Body {
 			get {
@@ -22,8 +35,15 @@ namespace LudumDare40 {
 			}
 		}
 
+		public Direction Facing {
+			get {
+				return facing;
+			}
+		}
+
 		void Start() {
 			body = GetComponent<Rigidbody2D> ();
+			animator = GetComponent<Animator> ();
 		}
 
 		// Update is called once per frame
@@ -36,7 +56,25 @@ namespace LudumDare40 {
 			if ((Mathf.Approximately (controls.x, 0) == false) || (Mathf.Approximately (controls.y, 0) == false)) {
 				controls.Normalize ();
 			}
-			//Debug.Log (controls);
+
+			// Update animation
+			animator.SetFloat (Velocity, controls.sqrMagnitude);
+			animator.SetFloat (Horizontal, controls.x);
+			animator.SetFloat (Vertical, controls.y);
+			if ((Mathf.Abs (controls.x) > 0.1f) || (Mathf.Abs(controls.y) > 0.1f)) {
+				if (Mathf.Abs (controls.x) > Mathf.Abs (controls.y)) {
+					facing = Direction.Left;
+					if (controls.x > 0) {
+						facing = Direction.Right;
+					}
+				} else {
+					facing = Direction.Down;
+					if (controls.y > 0) {
+						facing = Direction.Up;
+					}
+				}
+			}
+			animator.SetInteger (LastDirection, ((int)facing));
 		}
 
 		void FixedUpdate() {
@@ -45,7 +83,7 @@ namespace LudumDare40 {
 			move.y = controls.y * movementSpeed * Time.deltaTime;
 
 			// Apply force on the character
-			if (move.sqrMagnitude > 0) {
+			if (move.sqrMagnitude > 0.1f) {
 				body.velocity = move;
 			}
 		}
