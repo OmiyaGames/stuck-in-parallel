300ebed1d42499e9b85dcd3db442e88e19883165
Taro Omiya <taro.omiya@omiyagames.com>
2017-12-03 22:36:26 -0500

    Scripting first attempt at collider layer changes based on 3D collision.

---
+++ Assets/Project/Scripts/LanternZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LudumDare40 {
+	[RequireComponent(typeof(Collider))]
+	public class LanternZone : MonoBehaviour {
+		[SerializeField]
+		Lantern lantern;
+
+		void OnTriggerEnter(Collider other) {
+			DynamicCollider collider = DynamicCollider.GetScript (other);
+			if (collider != null) {
+				collider.OnEnterLantern (lantern);
+			}
+		}
+
+		void OnTriggerExit(Collider other) {
+			DynamicCollider collider = DynamicCollider.GetScript (other);
+			if (collider != null) {
+				collider.OnExitLantern (lantern);
+			}
+		}
+	}
+}
